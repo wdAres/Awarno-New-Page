30e55a68aabc2d26793671c547bd5b1c7aabbe65
wdAres <puneetsri9990@gmail.com>
2023-01-11 18:42:24 +0530

    ..

---
+++ script/index.js
@@ -0,0 +1,90 @@
+// Slider
+
+$("#slider-box").slick({
+  centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 4,
+  arrows: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 360,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+$("#slider-2").slick({
+  centerMode: true,
+  // centerPadding: '60px',
+  slidesToShow: 3,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 550,
+      settings: {
+        slidesToShow: 1,
+        arrows : false 
+      },
+    },
+    {
+      breakpoint: 360,
+      settings: {
+        slidesToShow: 1,
+        
+      },
+    }
+  ],
+});
